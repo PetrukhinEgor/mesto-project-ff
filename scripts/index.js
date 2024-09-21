c5b94af0e3d86741c73aa3cffa5059a12807929f
Professor_Egor <petruhin-egor.petruhin@yandex.ru>
2024-09-21 20:34:04 +0300

    Сдача ревьюеру 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(cardImageValue, cardTitleValue) {
+    // клонируем контент шаблона
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    // заполняем данными
+    cardElement.querySelector('.card__image').src = cardImageValue;
+    cardElement.querySelector('.card__title').textContent = cardTitleValue;
+
+    // добавляем в список (отображаем на странице)
+    cardList.append(cardElement);
+
+    // @todo: Функция удаления карточки
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function(event) {
+        cardElement.remove(); // удаление карточки
+    });
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item){
+    addCard(item.link, item.name)
+});
+
