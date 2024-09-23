d9fd3cabb3fef51ef4ffc1f1e071f91e670d04da
Professor_Egor <petruhin-egor.petruhin@yandex.ru>
2024-09-23 13:44:26 +0300

    Сдача ревьюеру 1

--- scripts/index.js
+++ scripts/index.js
@@ -3,42 +3,36 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const cardList = document.querySelector('.places__list');
-//===============
-const addCardButton = document.querySelector('.profile__add-button');
-const popUp = document.querySelector('.popup')
-
-
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
 // @todo: Функция создания карточки
-function addCard(cardImageValue, cardTitleValue) {
-    // клонируем контент шаблона
+function addCard(cardImageValue, cardTitleValue, deleteCard) {
+    // клонируем шаблон
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
     // заполняем данными
     cardElement.querySelector('.card__image').src = cardImageValue;
     cardElement.querySelector('.card__title').textContent = cardTitleValue;
 
-    
-    //обработчик события ДОБАВЛЕНИЯ КАРТОЧКИ(ДОДЕЛАТЬ)
-    addCardButton.addEventListener('click', function(){
-        popUp.setAttribute('style', 'display: flex')
-    });
-    //================================================
-
-
-
     // добавляем в список (отображаем на странице)
     cardList.append(cardElement);
+
+    const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+    cardDeleteButton.addEventListener('click', function(){
+        deleteCard(cardElement);
+    });
 }
 
-// @todo: Функция удаления карточки
-    // const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-    // cardDeleteButton.addEventListener('click', function(event) {
-    //     cardElement.remove(); // удаление карточки
-    // });
+//@todo: Функция удаления карточки
+function deleteCard(cardElement){
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(item){
-    addCard(item.link, item.name)
+    addCard(item.link, item.name, deleteCard);
 });
 
+
+//test
+//addCard(123,123, deleteCard)
